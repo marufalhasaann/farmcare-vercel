0f5f310742e6ff8e86db08a0f2d1681a2573ff26
marufalhasaann <marufalhasaann@gmail.com>
2026-04-30 02:11:18 +0600

    Delete farmcare3 directory

--- farmcare3/api/animals/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION['user'])) header("Location: ../index.php");
-require '../db.php';
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM animals WHERE id=$id");
-header("Location: list.php");
